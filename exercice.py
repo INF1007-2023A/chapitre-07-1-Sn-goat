7477427a7c31611d861ce3eb0bc3037ae201cae3
Sn-goat <sergile-junior.domguia-nyouvop@polymtl.ca>
2023-10-27 11:04:27 -0400

    argh

--- exercice.py
+++ exercice.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import math
+import turtle
 
 # TODO: Importez vos modules ici
+def mass_volume_ellipsoide(a,b,c,mass_density):
+    def volume_ellipsoide(a,b,c):
+        return (4/3)* math.pi * a * b * c
+    mass = volume_ellipsoide(a, b, c) * mass_density
+    return (volume_ellipsoide(a,b, c), mass)
+def arbre(nombre):
+    leo = turtle.Turtle()
+    if nombre == 0:
+        return(leo.forward(0))
+    else:
+        if nombre == 1:
+            return(leo.forward(1))
+        else:
+            return(arbre(nombre -1) + arbre(nombre - 2))
+    
+
 
 
 
@@ -10,5 +28,7 @@
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
+    print(mass_volume_ellipsoide(5,3,7, 0.7))
+    arbre(5)
 
     pass
